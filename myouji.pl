7b1c7383c13c05a1714ee100531de45124b9fad2
ONO Hiroki <onohiroki@cup.com>
2015-09-16 23:57:58 +0900

    jcode.pl をやめて Encode を使うようにした

--- myouji.pl
+++ myouji.pl
@@ -1,13 +1,35 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 # myouji.pl
 # 2001 copyright (c) おのひろき onohiroki@cup.com
-# 著作権はおのひろきにあります．自由に改変して自由に配布してくだされ．
+# 著作権はおのひろきにあります．
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2001 ONO Hiroi.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 # データベースなどで，なまえの姓名が分離していないと嫌な感じですよね．
 # 名字と名前の間に「,」をいれようとする perl スクリプトです．
-# つっくんの部屋 名字博士 http://www.alles.or.jp/‾tsuyama/name.htm から
+# つっくんの部屋 名字博士 http://www.alles.or.jp/~tsuyama/name.htm から
 # ダウンロードできる名字のデータファイル
-# http://www.alles.or.jp/‾tsuyama/image/myouji.lzh
+# http://www.alles.or.jp/~tsuyama/image/myouji.lzh
 # をもちいて，名前と名字が連結したものを分離しようとします．
 # 1 文字の名字よりは 2 文字の名字．2 文字の名字よりは 3 文字以上の
 # 名字にマッチングしようとします．同じ漢字なら，よりメジャーなフリガナ
@@ -26,7 +48,7 @@
 # という名字が登録されていると，出力は
 # 小野島,子,オノシマ
 # となります(苦笑)．
-# 
+#
 # 関守大 という名前だと，本当は「せきもり まさる」さんであっても
 # 「関守」という名字が名字データファイルに登録されていなくて，
 # 「関」という名字が登録されていれば，
@@ -34,111 +56,115 @@
 # と出力します．
 #
 # 適当な名字が見つけられないと，
-# おのひろき,error,error 
+# おのひろき,error,error
 # と出力されます．
 #
-# MacJPerl では，ドロップレットとして保存して，変換したいファイルを
-# ドラグ&ドロップします．
-#
-# UNIX や Windows の環境でもテストしました．data.txt という入力ファイルを
-# 用意したら
+# UNIX や Windows の環境でもテストしました． 
+# data.txt という入力ファイルを用意したら
 # myouji.pl data.txt
-# の用にコマンドで実行してみてください．
-#
-# jcode.pl を使う場合は，このスクリプト myouji.pl と同じディレクトリに
-# 置くか，perl のライブラリとして適切な場所に置いてください．
+# の様にコマンドで実行してみてください．
 #
 # 出力は，出力ファイルと標準出力にでます．標準出力側にはちらっと警告文など
 # も出るかもしれませんが，出力ファイルには余計なメッセージは含まれません．
-#
-# ぼくは perl についてまだ初心者ですから，なにか指摘してもらえたりすると
-# 嬉しいです．質問に対しては perl そのものの質問にはあまり答えられない
-# かもしれません．
 #
 # [つっくんの部屋 名字博士] はすばらしいです．5 万件ものデータが整理
 # されているのですから．データを公開してくださってありがとうございます．
 #
 # そんなこんなで自分が必要だからちょっと書いてみました．こんなものでも
 # あれば誰かの役にたつこともあるでしょう，きっと．
-# 
-# おのひろき onohiroki@cup.com 2001 年 1 月 31 日
+#
+# おのひろき onohiroki@cup.com
 # http://onohiroki.cycling.jp/
+# 2001-01-31 公開
+# 2015-09-16 修正しました．
 #
 ###########################################################################
 
-# 日本語の処理に jcode.pl の機能を一部利用します．
-# 漢字コード変換と半角カナ・全角カナの変換を行わない場合は，
-# とくに必要ないのですが，その場合は &jcode で始まる行を
-# コメントアウトするか削除してください．
-require 'jcode.pl';
- 
+use strict;
+use warnings;
+use utf8;
+use Encode qw/encode decode/;
+
 # 名字データファイル
-# つっくんの部屋 名字博士 http://www.alles.or.jp/‾tsuyama/name.htm
-# http://www.alles.or.jp/‾tsuyama/image/myouji.lzh
-$nameDB = "名字.txt"; 
+# つっくんの部屋 名字博士 http://www.alles.or.jp/~tsuyama/name.htm
+# http://www.alles.or.jp/~tsuyama/image/myouji.lzh
+# ダウンード後にファイル名を 名字.txt から nameDB.txt にリネームしてください．
+my $nameDB = "名字.txt";
+$nameDB = 'nameDB.txt';
 
-# 半角カナを全角に変換する場合には名字データファイルの漢字コードを
-# 指定します (euc | sjis | null) 
-$h2z = "sjis"; 
+#出力漢字コード Windows なら cp932．そうでなければ UTF-8 ．
+my $input_kanji  = 'cp932';
+my $output_kanji = 'cp932';
 
-$kanji = "sjis";           #出力漢字コード  (euc | sjis | jis)
-$outfile = "outfile.txt";  #出力ファイル
+# $output_kanji   = "UTF-8";
+if ( exists( $ENV{'LANG'} ) ) {
+    $output_kanji = 'UTF-8' if ( $ENV{'LANG'} =~ m/UTF/i );
+}
+my $outfile = 'outfile.txt';    #出力ファイル
 
 # プログラムはここから始まりです．
-$kw = "";
-$name1 = "";$name2 = "";$name3 = "";
-$yomi0 = "";$yomi1 = "";$yomi2 = "";$yomi3 = "";
-open(OUTFILE,">>$outfile");
+my $kw    = q{};
+my $name1 = q{};
+my $name2 = q{};
+my $name3 = q{};
+my $yomi0 = q{};
+my $yomi1 = q{};
+my $yomi2 = q{};
+my $yomi3 = q{};
+open( OUTFILE, ">", $outfile ) or die "error $!";
 print "start...\n";
-while($name = <>){
- &jcode'convert(*name, euc);
- $name =‾ s/\n//g;
-  $name =‾ s/\r//g;
-  open(DBFILE, "< $nameDB");
- $sw1 = 1;
-  while(($kw = <DBFILE> ) && $sw1){
-  if ($h2z eq "sjis"){
-   &jcode'h2z_sjis(*kw);
-  }elsif ($h2z eq "euc"){
-   &jcode'h2z_euc(*kw);
-  }
-  &jcode'convert(*kw, euc);
-    $kw =‾ s/\n//g;
-    $kw =‾ s/\r//g;
-    $kw =‾ s/([^,]+),([^,]+).*/$1/;
-  $yomi0 = $+; #print $kw;print $yomi0;print "\n";
-    if ($name =‾ /^$kw(.*)/) { 
-    if (length($kw) eq 2) { 
-     $name1 = "$kw,$+";
-     if ($name1 eq "" &&$ name2 eq "" && $name3 eq "") { 
-       &jcode'convert(*kw, $kanji);;
-              print "[$kw?]";}
-     if ($yomi1 eq "") {$yomi1 = $yomi0;}
+
+while ( my $name = <> ) {
+    $name = decode( $input_kanji, $name );
+    $name =~ s/\x0D|\x0A//gi;
+    open( DBFILE, "<", $nameDB ) or die "error $!";
+    my $sw1 = 1;
+    while ( my $kw = <DBFILE> ) {
+        last unless $sw1;
+        $kw = decode( $input_kanji, $kw );
+        $kw =~ s/\x0D|\x0A//gi;
+        $kw =~ s/([^,]+),([^,]+).*/$1/;
+        $yomi0 = $+;
+        if ( $name =~ /^$kw(.*)/ ) {
+            if ( length($kw) eq 2 ) {
+                $name1 = "$kw,$+";
+                if ( $name1 eq q{} && $name2 eq q{} && $name3 eq q{} ) {
+                    Jcode::convert( \$kw, $output_kanji );
+                    print "[$kw?]";
+                }
+                if ( $yomi1 eq q{} ) { $yomi1 = $yomi0; }
+            }
+            if ( length($kw) eq 4 ) {
+                $name2 = "$kw,$+";
+                if ( $yomi2 eq q{} ) { $yomi2 = $yomi0; }
+            }
+            if ( length($kw) > 4 ) {
+                $name3 = "$kw,$+";
+                if ( $yomi3 eq q{} ) { $yomi3 = $yomi0; }
+                $sw1 = 0;
+            }
+        }
     }
-    if (length($kw) eq 4) { 
-     $name2 = "$kw,$+";
-     if ($yomi2 eq "") {$yomi2 = $yomi0;}
+    close(DBFILE);
+    if ($sw1) { $name = "$name,error,error\n"; }
+    if ($name3) {
+        $name = "$name3,$yomi3\n";
     }
-    if (length($kw) > 4) {
-     $name3 = "$kw,$+";
-     if ($yomi3 eq "") {$yomi3 = $yomi0;}
-     $sw1 = 0;
+    elsif ($name2) {
+        $name = "$name2,$yomi2\n";
     }
-  }
- }
- close (DBFILE);
- if ($sw1) { $name = "$name,error,error\n";}
- if ($name3) {$name = "$name3,$yomi3\n";
- } elsif ($name2) {$name = "$name2,$yomi2\n";
- } elsif ($name1) {$name = "$name1,$yomi1\n"; }
- 
- $name1 = "";$name2 = "";$name3 = "";
- $yomi1 = "";$yomi2 = "";$yomi3 = "";
- &jcode'convert(*name, $kanji);
- print $name;
- print OUTFILE $name;
- # MacPerl や MacJPerl で　出力ファイルのファイルタイプを指定します．
- #&MacPerl'SetFileInfo('JEDT', 'TEXT', $outfile);
+    elsif ($name1) {
+        $name = "$name1,$yomi1\n";
+    }
+    $name1 = q{};
+    $name2 = q{};
+    $name3 = q{};
+    $yomi1 = q{};
+    $yomi2 = q{};
+    $yomi3 = q{};
+    print encode( $output_kanji, $name );
+    print OUTFILE encode( $output_kanji, $name );
 }
-close (OUTFILE);
+close(OUTFILE);
 print "End of script.\n";
+
